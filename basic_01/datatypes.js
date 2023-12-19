c45f14cd0a26e965ca7d7d0b8e20c3a11c078b8f
sujeet620 <154114647+sujeet620@users.noreply.github.com>
2023-12-19 06:57:38 +0000

    data types

---
+++ basic_01/datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; //this statements treate all 'js' code in new version.
+
+
+let age = 23;
+let name = "sujeet";
+let city = "Delhi";
+ let isfail = true;
+ let x;
+ let a = null;
+
+//alert("Hello!"); 
+/*this statement will not work because of we are using node.js
+as we know 'js' is browser side scripting langauge.
+*/
+
+/* 
+Data types:-
+
+    # primitive:-
+
+        1.Number -->it store integr type value +ve,-ve and decimal value.
+        2.String -->it is written inside the "" or '' .
+        3.boolean -->true/false
+        4.undefined -->it's declare the variable but the value of the variable is not assign.
+        5.null -->it is a repesentation of empty value.
+        6.bigint
+        7.symbol -->it is used for defining the unique value.
+
+    # Non-primitive:-
+        
+        object 
+
+*/
+
+console.log(typeof isfail); //obejct
+console.log(typeof name); //obejct
+console.log(typeof age); //obejct
+console.log(typeof null); //obejct
+console.log(typeof undefined); //undefined
