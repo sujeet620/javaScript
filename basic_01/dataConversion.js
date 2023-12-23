02bf54a1a1b4948741a011dda3a3b38a30a8faec
sujeet620 <154114647+sujeet620@users.noreply.github.com>
2023-12-23 04:42:38 +0000

    data type conversion

---
+++ basic_01/dataConversion.js
@@ -0,0 +1,49 @@
+//conversion of string into the number.
+let age = "26jhkd";
+// console.log(typeof age);
+// console.log(age);
+let valueInNumber = Number(age);
+console.log("typeof value is: " + typeof age);
+console.log(valueInNumber);
+
+//conversion of number into the string.
+let score = 25;
+let valueInString = String(score);
+console.log("Type of value is: " + typeof score);
+console.log(valueInString);
+
+//conversion of number into boolean.
+let isLoggedIn = "sujeet";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log("Type of value is: " + typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+  1 ==>true;   0 ==>false;
+  "" ==>false;  "sujeet" ==>true;
+*/
+
+
+//-------------------******************---------------  
+
+//Operations:-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//why string to number is confusing.
+
