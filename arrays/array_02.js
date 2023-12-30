63566aaf23d67ca40895746ffb59b465ce55fea0
sujeet620 <154114647+sujeet620@users.noreply.github.com>
2023-12-30 06:12:49 +0000

    more about array in js.

--- arrays/array_02.js
+++ arrays/array_02.js
@@ -1,10 +1,30 @@
 const heros = ["spiderman", "ironman", "thor"]
 const dc_heros = ["antman", "batman", "superman"]
 
-//here push method add array inside array.and push method return existing array.
-const allHeros = heros.push(dc_heros);
+// Appends/add new elements to the end of an array, and returns the new length of the array.
+//const allHeros = heros.push(dc_heros);
 // console.log(heros);
 
-//concat method is used for adding arrays and it will return new array.
-const newHeros=heros.concat(dc_heros);
-console.log(newHeros)
+//Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+//here is the limitations, we can only concat two array.
+const newHeros = heros.concat(dc_heros);
+//console.log(newHeros);
+
+//this is spread method which is used to concat the string.
+const all_new_heros = [...heros, ...dc_heros]
+//console.log(all_new_heros);
+
+
+const another_array = [1, 2, 4, [5, 6, 7], 8, 3,[2, 2, [1,2,3]] ]
+const real_array = another_array.flat(Infinity) ;
+//console.log(real_array);
+
+console.log(Array.isArray('sujeet'));
+console.log(Array.from("sujeet"));
+console.log(Array.from({name: "sujeet"}));
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1, score2, score3));
