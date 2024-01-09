eaa0eab726f4e812758b1d100b40a8acd99a049e
sujeet620 <154114647+sujeet620@users.noreply.github.com>
2024-01-09 12:32:13 +0000

    while and do while loop

---
+++ loops/loops_02.js
@@ -0,0 +1,21 @@
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index: ${index}`);
+    index +=2;
+}
+
+let myArray = ["shaktiman", "hero", "man"]
+let arr=0
+while (arr<myArray.length) {
+    
+    //console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+let score = 11;
+
+do {
+    console.log(`score is: ${score}`);
+    score++
+} while (score <= 10);
