6489a833cd956d4af309c5b6c1c858a0abfe1b9e
sujeet620 <154114647+sujeet620@users.noreply.github.com>
2024-01-09 05:17:28 +0000

    flow of programs

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -18,8 +18,26 @@ if (userEmail.length === 0) {
     
 }*/
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("obj is empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("obj is empty");
+// }
+
+//nullish coalescing Operator (??): null, undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+//val1 = undefined ?? 15;
+val1 = null ?? 10 ?? 15
+
+
+console.log(val1);
+
+//ternory Operator
+
+// Condition ? true : false;
+
+let iceTea = 150;
+iceTea <= 80 ? console.log("less than 80") : console.log("more than 80");
